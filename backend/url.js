46e799c0ce7abf57197a6bcdbf647517034988e7
aryan <comauro7511@gmail.com>
2024-07-13 01:40:19 +0530

    both frontend and backend successfully deployed

--- backend/url.js
+++ backend/url.js
@@ -1,3 +1,3 @@
-// export const frontend_url = "http://localhost:3000";
+// export const frontend_url  = "http://localhost:3000";
 
-export const frontend_url = 'https://azura-cart.vercel.app';
+export const frontend_url = 'https://azura-cart.vercel.app'; 
