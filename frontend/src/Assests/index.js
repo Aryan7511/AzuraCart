e6de451308b5e2d13f77de2bd692652f7b738730
aryan <comauro7511@gmail.com>
2024-07-19 14:35:37 +0530

    added minor changes

--- frontend/src/Assests/index.js
+++ frontend/src/Assests/index.js
@@ -8,6 +8,7 @@ import lg_logo from './Sponsered_Logo/LG-Logo-PNG1.png';
 import adidas_logo from './Sponsered_Logo/Adidas_Logo_PNG_(10).png';
 import asus_logo from './Sponsered_Logo/Asus-Logo-PNG6.png';
 import sony_logo from './Sponsered_Logo/Sony-Logo.png';
+import circularLoader from './cirular_loader.svg';
 
 const images = {
   Logo,
@@ -19,7 +20,8 @@ const images = {
   lg_logo,
   asus_logo,
   adidas_logo,
-  sony_logo
+  sony_logo,
+  circularLoader
 };
 
 export default images;
--- frontend/src/components/Route/FeaturedProduct/FeaturedProduct.jsx
+++ frontend/src/components/Route/FeaturedProduct/FeaturedProduct.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import styles from '../../../styles/styles';
 import ProductCard from '../ProductCard/ProductCard';
+import CircularLoader from '../../Layout/CircularLoader';
 
 const FeaturedProduct = () => {
-  const { allProducts } = useSelector((state) => state.products);
+  const { allProducts, isLoading } = useSelector((state) => state.products);
 
   return (
     <div>
@@ -12,16 +13,19 @@ const FeaturedProduct = () => {
         <div className={`${styles.heading}`}>
           <h1>Featured Products</h1>
         </div>
-        <div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-4 lg:gap-[25px] xl:grid-cols-5 xl:gap-[30px] mb-12 border-0">
-          {allProducts && allProducts.length !== 0 && (
-            <>
-              {allProducts &&
-                allProducts.map((i, index) => (
+        {isLoading ? (
+          <CircularLoader />
+        ) : (
+          <div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-4 lg:gap-[25px] xl:grid-cols-5 xl:gap-[30px] mb-12 border-0">
+            {allProducts && allProducts.length !== 0 && (
+              <>
+                {allProducts.map((i, index) => (
                   <ProductCard data={i} key={index} />
                 ))}
-            </>
-          )}
-        </div>
+              </>
+            )}
+          </div>
+        )}
       </div>
     </div>
   );
